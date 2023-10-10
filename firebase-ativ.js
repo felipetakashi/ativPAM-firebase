144305b5ed78df9117bbb65de3f88b6dc8b76f96
felipetakashi <femorota@gmail.com>
2023-10-10 20:13:42 -0300

    atualizacao

--- firebase-ativ.js
+++ firebase-ativ.js
@@ -1,16 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "sua-api-key",
-  authDomain: "seu-domínio.firebaseapp.com",
-  databaseURL: "https://seu-domínio.firebaseio.com",
-  projectId: "seu-projeto-id",
-  storageBucket: "seu-projeto-id.appspot.com",
-  messagingSenderId: "seu-id",
-  appId: "seu-app-id"
+  apiKey: "AIzaSyClUqFNz7JhKeZViJ6qouipfmy7ya2N2ik",
+  authDomain: "ativpam.firebaseapp.com",
+  projectId: "ativpam",
+  storageBucket: "ativpam.appspot.com",
+  messagingSenderId: "468602435855",
+  appId: "1:468602435855:web:6098790e587b25f80ea67a",
+  measurementId: "G-X7TW8B5WPQ"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
